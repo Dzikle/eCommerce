2fb6f0bf005ae4b4b1ffab98080c42e89d8ec86b
Stole Lazarov <72608012+Dzikle@users.noreply.github.com>
2020-12-22 15:24:06 +0100

    office

--- eCommerce/src/main/java/com/eCommerce/controller/ProductController.java
+++ eCommerce/src/main/java/com/eCommerce/controller/ProductController.java
@@ -2,19 +2,23 @@ package com.eCommerce.controller;
 
 import java.io.IOException;
 import java.util.Base64;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.eCommerce.entity.Product;
 import com.eCommerce.repository.ProductRepository;
+import com.eCommerce.services.UsersDetails;
 @Controller
 public class ProductController {
 	
@@ -48,6 +52,21 @@ public class ProductController {
 	public String updateProduct(Model model,@Param(value = "product") Product product) {
 		prodRepo.save(product);
 		return"updateForm";
+	}
+	
+	@GetMapping("/products")
+	public String productList(Model model) {
+		
+		List<Product> products = prodRepo.findAll();
+		model.addAttribute("products", products);
+		
+		return "productsList";
+	}
+	
+	@GetMapping("/delete/product/{id}")
+	public String deleteProduct(@PathVariable("id") Integer id) {
+		prodRepo.deleteById(id);		
+		return"redirect:/products";
 	}
 	
 
